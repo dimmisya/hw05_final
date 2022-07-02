29758e3a6c8b472e846b7f31c3fabe48c040ed2e
Дмитрий Мисюра <dimmis.ya@gmail.com>
2022-07-02 10:10:11 +0500

    second try

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -26,12 +26,12 @@ class PostCreateFormTests(TestCase):
             description='Тестовое описание',
         )
         cls.small_gif = (
-             b'\x47\x49\x46\x38\x39\x61\x02\x00'
-             b'\x01\x00\x80\x00\x00\x00\x00\x00'
-             b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
-             b'\x00\x00\x00\x2C\x00\x00\x00\x00'
-             b'\x02\x00\x01\x00\x00\x02\x02\x0C'
-             b'\x0A\x00\x3B'
+            b'\x47\x49\x46\x38\x39\x61\x02\x00'
+            b'\x01\x00\x80\x00\x00\x00\x00\x00'
+            b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+            b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+            b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+            b'\x0A\x00\x3B'
         )
         cls.uploaded = SimpleUploadedFile(
             name='small.gif',
@@ -67,9 +67,7 @@ class PostCreateFormTests(TestCase):
         self.assertTrue(
             Post.objects.filter(
                 text='Тестовый текст',
-                image='posts/small.gif'
-                ).exists()
-        )
+                image='posts/small.gif').exists())
 
     def test_edit_post(self):
         """Проверка формы редактирования поста."""
